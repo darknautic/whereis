60cdec031cbb0494753395e9caf9f0ddbe62c1e7
Octavio <octaviomadrigal@hotmail.com>
2018-04-20 16:37:00 -0700

    new where is

---
+++ whereis.py
@@ -0,0 +1,8 @@
+#print tavo corrigelo y guardalo please
+]ya se guardo >? si con ctrl+S
+oks
+ahora puedes abrir git solo para mostrattye algo git sitio o app?
+console
+print(tavo)
+cual d elos 3?
+CMD GUI o bashgitbash
