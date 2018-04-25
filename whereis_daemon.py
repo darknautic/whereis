4cdae15d8171deba948ed630ef64f82724261ccc
darkNautic <blue.darknautic@gmail.com>
2018-04-25 17:42:45 -0500

    decreasing directory levels

--- whereis_daemon.py
+++ whereis_daemon.py
@@ -72,6 +72,7 @@ def build_index():
         indexing(path)
         print("Indexing \"" + path + " \" ... ")
 
+    print("[INFO] - Index Size : ", len( index_cache))
     print("[INFO] - Total Indexing Time : ", time.time() - t_start )
 
 
